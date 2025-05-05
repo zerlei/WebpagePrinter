95fbbd2a75188657b0dbb8afc0f9162c2b9eb310
zerlei <1445089819@qq.com>
2025-05-05 21:51:12 +0800

    chore: doxygen support mermaid and plantuml now

--- src/log.h
+++ src/log.h
@@ -25,7 +25,17 @@ public:
   }
 
 private:
+  //! @mermaid
+  //! flowchart TD
+  //!     A[Christmas] -->|Get money| B(Go shopping)
+  //!     B --> C{Let me think}
+  //!     C -->|One| D[Laptop]
+  //!     C -->|Two| E[iPhone]
+  //!     C -->|Three| F[fa:fa-car Car]
+  //! @endmermaid
   static inline QFile logFile;
+
+  
   static inline QMutex mutex;
   static inline QString logFilePath;
   static inline qint64 maxSize = 5 * 1024 * 1024;
